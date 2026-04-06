8474ba836d3800090d52c4c0abdc09d1ce1d4cc8
chaeyuseong@naver.com <spitz910325@naver.com>
2026-04-06 19:38:57 +0900

    gitignore 파일 추가

---
+++ CH1_Custom_types_and_Swift_Testing/CH1_Custom_types_and_Swift_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CH1_Custom_types_and_Swift_TestingApp.swift
+//  CH1_Custom_types_and_Swift_Testing
+//
+//  Created by 채유성 on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CH1_Custom_types_and_Swift_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CH1_Custom_types_and_Swift_Testing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CH1_Custom_types_and_Swift_Testing
+//
+//  Created by 채유성 on 4/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
